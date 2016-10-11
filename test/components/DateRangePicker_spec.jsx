rocketrip/react-dates

Add directionAnchor prop to allow right-aligned Components
DateRangePicker and SingleDatePicker have a new prop, directionAnchor
that will align the components to the left/right side of the page.
This prop has two new constants, DIRECTION_ANCHOR_LEFT (default)
and DIRECTION_ANCHOR_RIGHT.

test/components/DateRangePicker_spec.jsx
@@ -19,6 +19,8 @@ import {
   VERTICAL_ORIENTATION,
   START_DATE,
   END_DATE,
+  DIRECTION_ANCHOR_LEFT,
+  DIRECTION_ANCHOR_RIGHT,
 } from '../../src/constants';
 
 const today = moment().startOf('day');
@@ -66,6 +68,20 @@ describe('DateRangePicker', () => {
       it('renders <DayPicker /> with props.numberOfMonths === 2', () => {
         const wrapper = shallow(<DateRangePicker orientation={HORIZONTAL_ORIENTATION} />);
         expect(wrapper.find(DayPicker).props().numberOfMonths).to.equal(2);
+      });
+    });
+
+    describe('props.directionAnchor === DIRECTION_ANCHOR_LEFT', () => {
+      it('renders .DateRangePicker__picker--direction-left class', () => {
+        const wrapper = shallow(<DateRangePicker directionAnchor={DIRECTION_ANCHOR_LEFT} />);
+        expect(wrapper.find('.DateRangePicker__picker--direction-left')).to.have.length(1);
+      });
+    });
+
+    describe('props.orientation === DIRECTION_ANCHOR_RIGHT', () => {
+      it('renders .DateRangePicker__picker--direction-right class', () => {
+        const wrapper = shallow(<DateRangePicker directionAnchor={DIRECTION_ANCHOR_RIGHT} />);
+        expect(wrapper.find('.DateRangePicker__picker--direction-right')).to.have.length(1);
       });
     });
 

alocpro/react-date

test/components/DateRangePicker_spec.jsx
@@ -19,6 +19,8 @@ import {
   VERTICAL_ORIENTATION,
   START_DATE,
   END_DATE,
+  ANCHOR_LEFT,
+  ANCHOR_RIGHT,
 } from '../../constants';
 
 const today = moment().startOf('day');
@@ -66,6 +68,20 @@ describe('DateRangePicker', () => {
       it('renders <DayPicker /> with props.numberOfMonths === 2', () => {
         const wrapper = shallow(<DateRangePicker orientation={HORIZONTAL_ORIENTATION} />);
         expect(wrapper.find(DayPicker).props().numberOfMonths).to.equal(2);
+      });
+    });
+
+    describe('props.anchorDirection === ANCHOR_LEFT', () => {
+      it('renders .DateRangePicker__picker--direction-left class', () => {
+        const wrapper = shallow(<DateRangePicker anchorDirection={ANCHOR_LEFT} />);
+        expect(wrapper.find('.DateRangePicker__picker--direction-left')).to.have.length(1);
+      });
+    });
+
+    describe('props.orientation === ANCHOR_RIGHT', () => {
+      it('renders .DateRangePicker__picker--direction-right class', () => {
+        const wrapper = shallow(<DateRangePicker anchorDirection={ANCHOR_RIGHT} />);
+        expect(wrapper.find('.DateRangePicker__picker--direction-right')).to.have.length(1);
       });
     });
 